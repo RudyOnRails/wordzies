bf03f686e722dd792d45318bcb8c374c0f2f003c
Kevin Musiorski <musiorski@gmail.com>
2012-02-10 19:25:04 -0600

    initial commit of new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordziesCA::Application.config.secret_token = 'bd0e7578d970c675a43091162b04281dad76903d565d43d410bcefdc1266bd41653d340cf01d4c995d31e6e00435342fb1850d8b10308f31972d3347c8eea141'
