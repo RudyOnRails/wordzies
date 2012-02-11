0688d8338e7379fe0c241a794ec3e491dc196510
Ben Block <bjblock@gmail.com>
2012-02-11 16:21:33 -0600

    fixed wn_title issue and words table submit now works

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120211215127) do
+ActiveRecord::Schema.define(:version => 20120211221953) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(:version => 20120211215127) do
   create_table "words", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "wn_title"
     t.integer  "wn_id"
+    t.string   "wn_title"
   end
 
 end
