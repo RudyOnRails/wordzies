ffd6a912f8df3adade89f14fff40590dc3f849d6
Ben Block <bjblock@gmail.com>
2012-02-11 19:51:58 -0600

    commented out broken has_attached_file method

--- app/models/use.rb
+++ app/models/use.rb
@@ -2,5 +2,5 @@ class Use < ActiveRecord::Base
   belongs_to :word
   belongs_to :user
   
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  # has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
