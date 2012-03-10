d34cc50d1343b680e38ec441975ab0ca0bfad402
Kevin Musiorski <musiorski@gmail.com>
2012-03-10 13:53:24 -0600

    deleted some garbage in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,18 @@
 WordziesCA::Application.routes.draw do
-
-  get "uses/create"
-
+  
   root :to => 'words#index'
-  # resources :words
+  
+  resources :words
   # get "words/index"
   # resources :words, :only => [:index]
-
-
-
-  resources :words
   get '/search' => 'words#search'
   get 'words/:word' => 'words#show', :as => 'show_word'
   #post '/words' => 'words#create'
   
-  
-  #resources :words
   resources :users
+  
   resources :uses
+  get "uses/create"
   
   match '/auth/twitter/callback', to: 'sessions#create'
 
