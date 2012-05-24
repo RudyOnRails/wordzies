348556f533e1ed10d14a9bc6bc98af280324ab10
Kevin Musiorski <musiorski@gmail.com>
2012-05-23 19:31:21 -0500

    trying to fix redirect_to :back issue in prod

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     redirect_to :back, :notice => "Signed in!"
+    rescue ActionController::RedirectBackError
+    redirect_to root_path
   end
   
   def destroy
